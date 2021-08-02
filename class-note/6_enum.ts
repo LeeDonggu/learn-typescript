1a1896934488fe2908675255078c711525e5ad50
LeeDonggu <7sus7@naver.com>
2021-08-02 13:49:35 +0900

    enum 숫자형, 문자형

---
+++ class-note/6_enum.ts
@@ -0,0 +1,10 @@
+// enum 타입을 사용할 때 속성의 값을 초기화 하지 않으면 숫자로 초기화됨
+// 초기화를 하지 않으면 0부터 1씩 숫자가 증가해서 초기값이 지정됨
+// 숫자로 초기화하면 초기화 값부터 증가
+enum Shoes {
+  Nike = "나이키",
+  Adidas = "아디다스",
+}
+
+var myShoes = Shoes.Nike;
+console.log(myShoes); // 0
