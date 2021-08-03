7fa839ec16f8b33612d8c6f84b41f40c91bc4cc6
LeeDonggu <7sus7@naver.com>
2021-08-03 13:15:22 +0900

    기존 타입 정의 방식과 제네릭의 차이점 - 함수 중복 선언 단점

---
+++ class-note/8_generics.ts
@@ -0,0 +1,30 @@
+// function logText(text) {
+//   console.log(text);
+//   return text;
+// }
+
+// logText(10); // 숫자 10
+// logText("하이"); // 문자열 하이
+// logText(true); //진위값 true
+
+// function logText<T>(text: T) {
+//   console.log(text);
+//   return text;
+// }
+// logText<string>("하이");
+
+function logText(text: string) {
+  console.log(text);
+  //   text.split("").reverse().join("");
+  return text;
+}
+
+function logNumber(num: number) {
+  console.log(num);
+  return num;
+}
+
+logText("a");
+logText(10);
+const num = logNumber(10);
+logText(true);
