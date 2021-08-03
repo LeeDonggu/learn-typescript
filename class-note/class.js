ec2feec6b977500b5dbefb82f2746a4c13d83fca
LeeDonggu <7sus7@naver.com>
2021-08-03 12:56:46 +0900

    프로토타입과 클래스의 관계

--- class-note/class.js
+++ class-note/class.js
@@ -1,5 +1,13 @@
 // ES2015 (ES6) 이후부터 나온 문법
+// 생성자 패턴을 이용해서 class 문법을 제공하는것
+// 아래의 두가지 문법이 동일함
 
+function Persion(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+var dg = new Person('동구', 100);
 class Person {
   // 클래스 로직
   constructor(name, age) {
