ee2a68eeb6975ec8afd3bebed83cc0aa3e168768
LeeDonggu <7sus7@naver.com>
2021-09-14 13:44:18 +0900

    타입 호환 함수, 제네릭

--- class-note/12_type-compatibility.ts
+++ class-note/12_type-compatibility.ts
@@ -8,6 +8,7 @@ interface Developer {
 // }
 class Person {
   name: string;
+  skill: string;
 }
 
 var developer: Developer;
@@ -15,3 +16,30 @@ var person: Person;
 // developer = person;
 person = developer;
 developer = new Person();
+
+// 함수
+var add = function (a: number) {
+  // ...
+};
+var sum = function (a: number, b: number) {
+  // ...
+};
+sum = add;
+// add = sum;
+
+// 제네릭
+interface Empty<T> {
+  // ..
+}
+var empty1: Empty<string>;
+var empty2: Empty<number>;
+empty1 = empty2;
+empty2 = empty1;
+
+interface NotEmpty<T> {
+  data: T;
+}
+var notempty1: NotEmpty<string>;
+var notempty2: NotEmpty<number>;
+notempty1 = notempty2;
+notempty2 = notempty1;
