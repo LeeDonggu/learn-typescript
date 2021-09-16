af8fab6b0495ecb6ee75713e3117a7cf49075492
LeeDonggu <7sus7@naver.com>
2021-09-16 13:59:00 +0900

    유틸리티 타입(Partial) 직접 구현(맵드 타입 이용)

--- class-note/13_utility-type.ts
+++ class-note/13_utility-type.ts
@@ -31,3 +31,36 @@ function displayProductDetail(shoppingItem: ShoppingItem) {}
 type UpdateProduct = Partial<Product>;
 // 3. 특정 상품 정보를 업데이트(갱신) 하는 함수
 function updateProductItem(productItem: UpdateProduct) {}
+
+// 4. 유틸리티 타입 구현하기 - Partial
+interface UserProfile {
+  username: string;
+  email: string;
+  profilePhotoUrl: string;
+}
+// interface UserProfileUpdate {
+//   username?: string;
+//   email?: string;
+//   profilePhotoUrl?: string;
+// }
+// #1
+// type UserProfileUpdate = {
+//   username?: UserProfile["username"];
+//   email?: UserProfile["email"];
+//   profilePhotoUrl?: UserProfile["profilePhotoUrl"];
+// };
+// // #2 (맵드 타입)
+// type UserProfileUpdate = {
+//   [p in "username" | "email" | "profilePhotoUrl"]?: UserProfile[p];
+// };
+type UserProfileKeys = keyof UserProfile;
+
+// #3
+type UserProfileUpdate = {
+  [p in keyof UserProfile]?: UserProfile[p];
+};
+
+// 4
+type Subset<T> = {
+  [p in keyof T]?: T[p];
+};
