25bdf257f9f979b635f92d80593cd11a2feaf154
LeeDonggu <7sus7@naver.com>
2021-09-16 13:50:20 +0900

    유틸리티 타입(Partial)

--- class-note/13_utility-type.ts
+++ class-note/13_utility-type.ts
@@ -6,11 +6,28 @@ interface Product {
   stock: number;
 }
 
-// 상품 목록을 받아오기 위한 API 함수
+// 1.상품 목록을 받아오기 위한 API 함수
 function fetchProducts(): Promise<Project[]> {}
 
-type ShoppingItem = Pick<Product, "id" | "name" | "price">;
+// interface ProductDetail {
+//   id: number;
+//   name: string;
+//   price: number;
+// }
 
+// 2. 특정 상품의 상세 정보를 타나태기 위한 함수
+type ShoppingItem = Pick<Product, "id" | "name" | "price">;
 function displayProductDetail(shoppingItem: ShoppingItem) {}
 
 // Omit은 Pick의 반대로 동작
+
+// interface UpdateProduct {
+//   id?: number;
+//   name?: string;
+//   price?: number;
+//   brand?: string;
+//   stock?: number;
+// }
+type UpdateProduct = Partial<Product>;
+// 3. 특정 상품 정보를 업데이트(갱신) 하는 함수
+function updateProductItem(productItem: UpdateProduct) {}
